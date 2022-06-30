f5dde727ec9aa206e3eae5a6f3758691d5d37022
Samuel Larcome <87437514+samlarcome@users.noreply.github.com>
2022-06-30 16:29:47 -0400

    Create globals.py

---
+++ globals.py
@@ -0,0 +1,14 @@
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
+    }
+
+arizonaMDLookupURL = "https://azbomprod.azmd.gov/GLSuiteWeb/Clients/AZBOM/public/WebVerificationSearch.aspx?q=azmd&t=20220622082816"
+
+
+# Return to see if you can dynamically find these values
+specialtyPostValues = { "AnesthesiologySpecialtyCode":"12155",  
+                        "SurgerySpecialtyCode":"12357",
+                        "Family MedicineSpecialtyCode": "12198",
+                        "PsychiatrySpecialtyCode": "12329",
+                        "RadiologySpecialtyCode": "12342",
+                        "Surgical OncologySpecialtyCode": "12359"}
